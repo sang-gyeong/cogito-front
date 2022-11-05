6ca4e82e48ffe281b3452d2c79788725ce99d2e6
summer(이상경) <summer.0916@kakaocommerce.com>
2022-11-05 16:31:54 +0900

    chore: next.js 로 마이그레이트 &  보일러플레이트 생성
    자체 빌드툴이 존재하는 next.js와 vite를 함께 사용하기 어려움이 있다고 느꼈고,
    개발 질의응답 커뮤니티라는 서비스 특성상 SEO의 장점을 최대한 살리기 위해 SSR에 기반한 next.js를 최종 기술스택으로 채택함.
    (css 툴로는 bootstrap, styled-components 채택)

---
+++ pages/_document.tsx
@@ -0,0 +1,55 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossOrigin="anonymous"
+            async
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
