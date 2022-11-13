36092ec0c0d10633d3460167b4c2fd14c90ea8cd
summer(이상경) <summer.0916@kakaocommerce.com>
2022-11-13 23:50:34 +0900

    feat: react-query 프로바이더 주입 & queryClient 인스턴스 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,54 @@
-import type {AppProps} from 'next/app';
-import {ReactElement, ReactNode} from 'react';
-import {NextPage} from 'next';
+import '@uiw/react-md-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
+import {ReactElement, ReactNode, useState} from 'react';
+import {AppProps} from 'next/app';
 import Head from 'next/head';
+import {Hydrate, DehydratedState, QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import ThemedGlobalStyle from '../src/theme/ThemedGlobalStyle';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
-type NextPageWithLayout = NextPage & {
+type NextPageWithLayout = AppProps & {
   getLayout: (page: ReactElement) => ReactNode;
+};
+
+type PageProps = {
+  dehydratedState?: DehydratedState;
 };
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
+  pageProps: PageProps;
 };
 
 function MyApp({Component, pageProps}: AppPropsWithLayout) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            onError: error => console.log(error),
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+          },
+        },
+      })
+  );
   const getLayout = Component.getLayout ?? (page => page);
 
   return (
     <>
       <Head>
         <title>Cogito : 나는 생각한다</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="preload" href="/api/data" as="fetch" crossorigin="anonymous" /> */}
       </Head>
-      <ThemedGlobalStyle />
-      {getLayout(<Component {...pageProps} />)}
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemedGlobalStyle />
+          {getLayout(<Component {...pageProps} />)}
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
