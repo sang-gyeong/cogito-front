90ac0ea26242f88f96452dfaab1c2ea43234d440
summer(이상경) <summer.0916@kakaocommerce.com>
2023-01-16 16:12:38 +0900

    fix: no-unescaped-entities 수정

--- src/components/TagItem/index.tsx
+++ src/components/TagItem/index.tsx
@@ -12,7 +12,7 @@ export default function TagItem({tag}: {tag: Tag}) {
       <Card.Body>
         {/* <Card.Title>Card Title</Card.Title> */}
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          Some quick example text to build on the card title and make up the bulk of the cards content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
--- src/components/UserItem/index.tsx
+++ src/components/UserItem/index.tsx
@@ -13,7 +13,7 @@ export default function UserItem({user}: {user: User}) {
       <Card.Body>
         {/* <Card.Title>Card Title</Card.Title> */}
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          Some quick example text to build on the card title and make up the bulk of the cards content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
