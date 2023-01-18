6f5a3f05c00782ef7df2ab1db2e8af8a1df787b0
summer(이상경) <summer.0916@kakaocommerce.com>
2023-01-18 22:34:10 +0900

    fix: 잘못된 임포트명 수정

--- src/api/commnet.ts
+++ src/api/commnet.ts
@@ -1,4 +1,4 @@
-import request from './index';
+import {axiosInstanceForCSR, axiosInstanceForSSR} from './index';
 
 interface ModifyPostItem {
   title: string;
@@ -8,12 +8,15 @@ interface ModifyPostItem {
 }
 
 export const getPosts = (query?: string, page?: number, size?: number): Promise<Post.ListItemResponse> => {
-  return request.get<Post.ListItemResponse>(`/posts`, {params: {query, page, size}}).then(response => response?.data);
+  return axiosInstanceForSSR
+    .get<Post.ListItemResponse>(`/posts`, {params: {query, page, size}})
+    .then(response => response?.data);
 };
 
 export const getPostById = (id: number): Promise<Post.Item> =>
-  request.get<Post.Item>(`/posts/${id}`).then(response => response?.data);
+  axiosInstanceForSSR.get<Post.Item>(`/posts/${id}`).then(response => response?.data);
 
-export const modifyPost = (id: number, body: ModifyPostItem): Promise<{}> => request.patch(`/posts/${id}`, body);
+export const modifyPost = (id: number, body: ModifyPostItem): Promise<{}> =>
+  axiosInstanceForCSR.patch(`/posts/${id}`, body);
 
-export const deletePostById = (id: number): Promise<{}> => request.delete(`/posts/${id}`);
+export const deletePostById = (id: number): Promise<{}> => axiosInstanceForCSR.delete(`/posts/${id}`);
