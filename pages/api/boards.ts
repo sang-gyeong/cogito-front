0f8c58e5524ceb28d9d11166daedf2b704936984
summer(이상경) <summer.0916@kakaocommerce.com>
2023-01-26 00:39:22 +0900

    chore: api 라우팅 파일 제거

--- pages/api/boards.ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {NextApiRequest, NextApiResponse} from 'next';
-import {MOCK_UP_BOARDS} from '../../src/constants/data';
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse<Board[]>) {
-  res.status(200).json(MOCK_UP_BOARDS);
-}
--- pages/api/users.ts
+++
@@ -1,7 +0,0 @@
-import {MOCK_UP_USERS} from './../../src/constants/data';
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {NextApiRequest, NextApiResponse} from 'next';
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse<User[]>) {
-  res.status(200).json(MOCK_UP_USERS);
-}
