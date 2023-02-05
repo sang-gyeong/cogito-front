ca04641010e155d8898a9832484ff8246d314711
summer(이상경) <summer.0916@kakaocommerce.com>
2023-02-05 18:17:07 +0900

    chore: .next 업로드

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,10 +1,6 @@
 import {ReactElement} from 'react';
 import Layout from '../src/components/Layout';
-import HomeTemplate from '../src/components/templates/HomeTemplate';
-import {QueryClient, dehydrate} from '@tanstack/react-query';
-import {GetServerSideProps} from 'next';
-import {getUsers} from '../src/api';
-import {QUERY_KEY} from '../src/queries/useTagsQuery';
+
 import UserTemplate from '../src/components/templates/UserTemplate';
 
 export default function UserPage() {
@@ -15,14 +11,14 @@ UserPage.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const queryClient = new QueryClient();
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery([QUERY_KEY], getUsers);
+//   await queryClient.prefetchQuery([QUERY_KEY], getUsers);
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
